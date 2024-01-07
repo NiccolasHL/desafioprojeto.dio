a7391b74f529c5b8a206ae0a36447110d97d3bf1
Níccolas Lopes <nicco.lopes97@gmail.com>
2024-01-07 16:13:58 -0300

    Desafio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-console.log("Hello World")
-var teste = 1
+var nomePers = "Niccla Da Guerra"
+var expPers = 1001
+
+if (expPers <= 1000){
+    console.log("O herói de nome " + nomePers + "está no nível Ferro ")
+}
+else if (expPers <=2000){
+    console.log("O herói de nome " + nomePers + "está no nível Bronze ")
+}
+else if (expPers <=5000){
+    console.log("O herói de nome " + nomePers + "está no nível Prata ")
+}
+else if (expPers <=7000){
+    console.log("O herói de nome " + nomePers + "está no nível Ouro ")
+}
+else if (expPers <=8000){
+    console.log("O herói de nome " + nomePers + "está no nível Platina ")
+}
+else if (expPers <=9000){
+    console.log("O herói de nome " + nomePers + "está no nível Ascendente ")
+}
+else if (expPers <=10000){
+    console.log("O herói de nome " + nomePers + "está no nível Imortal ")
+}
+else {
+    console.log("O herói de nome " + nomePers + "está no nível Radiante ")
+}
